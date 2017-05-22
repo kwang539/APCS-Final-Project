66467c44e7bac2b9f63b234ee5e96fcfb26a517f
jyoungsports <jyoung576@student.fuhsd.org>
2017-05-21 22:55:55 -0700

    Update MenuPanel.java

--- src/panels/MenuPanel.java
+++ src/panels/MenuPanel.java
@@ -39,6 +39,7 @@ public class MenuPanel extends JPanel implements ActionListener {
 		if(e.getSource().equals(OptionsButton))
 			w.changePanel("3");
 		if(e.getSource().equals(playButton)){
+			a.stopSound4();
 //			w.cl.removeLayoutComponent(w.panel4);
 //			w.panel4 = new GamePanel(w);
 //			w.cardPanel.add(w.panel4, "4");
