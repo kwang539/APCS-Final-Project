21d1051d1dd7a5587ed42932bfeef1cc7d320b1c
jyoungsports <jyoung576@student.fuhsd.org>
2017-05-21 22:55:10 -0700

    Update Link.java

--- src/gameparts/Link.java
+++ src/gameparts/Link.java
@@ -7,12 +7,13 @@ import javax.swing.Timer;
 
 
 public class Link implements ActionListener {
-
-	private final int ACTION_TIMEOUT = 500;
+private final int ACTION_TIMEOUT = 500;
 
 	// Load the sounds from the disk. 
 	private final EasySound2 sound1 = new EasySound2("Sound1.wav");
 	private final EasySound2 sound2 = new EasySound2("Sound2.wav");
+	private final EasySound2 sound3 = new EasySound2("Sound3.wav");
+	private final EasySound2 sound4 = new EasySound2("Sound4.wav");
 
 
 	private JComponent surface; // for repainting when we make changes to his image
@@ -35,6 +36,19 @@ public class Link implements ActionListener {
 		actionTimer.restart();
 	}
 
+	public void sound3(){
+		sound3.play();
+	}
+	
+	public void sound4(){
+		sound4.play();
+		
+	}
+	
+	public void stopSound4(){
+		actionTimer.stop();
+	}
+	
 	public void actionPerformed(ActionEvent e) {
 		surface.repaint();
 		
